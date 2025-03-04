1ba82cead0dd2d4a6be60cf4a3978c337306b972
AdiBenZur <adi.benzur@gmail.com>
2025-03-04 16:18:32 +0200

    server test

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("hi")
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
